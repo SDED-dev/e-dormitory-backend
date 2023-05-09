3ffecd840398c54149b897481b88f4126d57351c
Sined <denis2320032@gmail.com>
2023-05-09 11:13:27 +0300

    🍃 | Додано параметри для отримання списку

--- controllers/user/list.js
+++ controllers/user/list.js
@@ -2,9 +2,16 @@ const db = require($ + "/db.js");
 
 module.exports = async (req, res) => {
   try {
-    const data = await db(
-      "SELECT users.id, email, phone FROM users INNER JOIN user_roles ON users.id = user_roles.user_id WHERE role_id = 2"
-    );
+    const { filter } = req.query;
+
+    var query =
+      "SELECT users.id, email, phone FROM users INNER JOIN user_roles ON users.id = user_roles.user_id";
+
+    if (!filter) query += " WHERE role_id = 2 OR role_id = 3";
+    if (filter == "faculty_admin") query += " WHERE role_id = 2";
+    if (filter == "dormitory_admin") query += " WHERE role_id = 3";
+
+    const data = await db(query);
     res.status(200).json(data);
   } catch (err) {
     console.log(err);
