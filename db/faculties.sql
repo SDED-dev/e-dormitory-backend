8f52f33f7ea38bd6c9733a43014b3863caf66e52
Sined <denis2320032@gmail.com>
2023-05-10 00:17:21 +0300

    🗑 | Видалено не потрібні файли

--- db/faculties.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE
-    `faculties` (
-        `id` int NOT NULL AUTO_INCREMENT,
-        `name` varchar(32) DEFAULT NULL UNIQUE,
-        PRIMARY KEY (`id`)
-    );
--- db/users.sql
+++
@@ -1,8 +0,0 @@
-create table
-    users(
-        id int not null auto_increment unique,
-        email varchar(64) not null unique,
-        phone varchar(64) not null unique,
-        password varchar(64) not null,
-        PRIMARY KEY (`id`)
-    );
