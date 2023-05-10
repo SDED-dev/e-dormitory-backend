9111280e8c8fdeb8b0e8718af525cda2cdcf493d
Sined <denis2320032@gmail.com>
2023-05-10 11:57:48 +0300

    🍂 | Змінено вихідний список гуртожитків

--- controllers/dormitory/list.js
+++ controllers/dormitory/list.js
@@ -5,6 +5,16 @@ module.exports = async (req, res) => {
     const data = await db(
       "SELECT dormitories.id, number, commandant_id, email  FROM dormitories LEFT JOIN users ON dormitories.commandant_id = users.id"
     );
+    for (let i = 0; i < data.length; i++) {
+      var temp = await db(
+        `SELECT faculties_id FROM faculties_dormitory WHERE dormitory_id = ?`,
+        [data[i].id]
+      );
+      data[i].faculties = [];
+      for (let j = 0; j < temp.length; j++) {
+        data[i].faculties.push(temp[j].faculties_id);
+      }
+    }
     res.status(200).json(data);
   } catch (err) {
     console.log(err);
